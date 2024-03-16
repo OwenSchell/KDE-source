dc62c41f80a74b514f205dc428857fc859d9e564
NhatAnh <hoangnhatanh472005@gmail.com>
2024-03-16 15:36:55 -0800

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function App() {
+  return (
+    <>
+    <Card name="Sena" age={25} profession="Developer"></Card>
+
+    </>
+  );
+}
+
+export default App
---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,17 @@
+import profilePic  from "./assets/march7th.jpg"
+
+function Card(props) {
+    return(
+        <div className="login-container">
+            <div className="login-form">
+                <h2>Sign In</h2>
+                <input type="text" placeholder="Username"/>
+                <input type="text" placeholder="Password"/>
+                <p>Forgot your password ?</p>
+                <button>Login</button>
+            </div>
+        </div>
+    );
+}
+
+export default Card
